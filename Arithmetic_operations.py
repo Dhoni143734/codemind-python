c72711721ccc23c6e6318cb5600e786af181b5bc
Dhoni143734 <147475366+Dhoni143734@users.noreply.github.com>
2024-08-12 10:07:25 +0530

    import math a=int(input()) b=int(input()) print("Sum:{}".format (a+b)) print("Difference:{}".format (a-b)) print("Product:{}".format (a*b)) print("Quotient:{}".format (math.floor(a/b))) print("Remainder:{}".format (a%b))

---
+++ Arithmetic_operations.py
@@ -0,0 +1,8 @@
+import math
+a=int(input())
+b=int(input())
+print("Sum:{}".format (a+b))
+print("Difference:{}".format (a-b))
+print("Product:{}".format (a*b))
+print("Quotient:{}".format (math.floor(a/b)))
+print("Remainder:{}".format (a%b))
